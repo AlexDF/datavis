319b71278320b6eec7ec976915d57d9595d1b6cc
Alexandre DeFreitas <abd9@njit.edu>
2014-04-21 00:58:29 -0400

    added code to load csv file and print records to console log.

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require('express');
+var fs = require('fs');
+var csv = require('csv');
+var records = new Array();
 
 app = express();
+
+csv(records)
+  .from.stream(fs.createReadStream('data' + '/directory.csv'), {
+  columns: true
+})
+  .on('record', function(row, index) {
+  records.push(row);
+
+  console.log(row);
+})
 
 app.get( '/', function(req, res) {
   res.send("hello");
