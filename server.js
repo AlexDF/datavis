76725c1cb6e51cb3bc30b83c56e0bbaedab87b37
Alexandre DeFreitas <abd9@njit.edu>
2014-04-21 01:37:23 -0400

    added initial setup for mongodb/mongoose connection

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var fs = require('fs');
 var csv = require('csv');
 var records = new Array();
 
 app = express();
+
+mongoose.connect('mongodb://localhost/directory');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+  // code goes here
+});
+
 
 csv(records)
   .from.stream(fs.createReadStream('data' + '/directory.csv'), {
@@ -12,11 +21,11 @@ csv(records)
   .on('record', function(row, index) {
   records.push(row);
 
-  console.log(row);
-})
+  //console.log(row);
+});
 
 app.get( '/', function(req, res) {
-  res.send("hello");
+  res.send(records);
 });
 
 app.listen(3000);
