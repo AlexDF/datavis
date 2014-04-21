d9d8279383839a31ea439b0291cb4cb9068cd2d9
Alexandre DeFreitas <abd9@njit.edu>
2014-04-21 16:56:19 -0400

    included code to save all csv records to mongodb. Afterwards, I commented it all out.

--- server.js
+++ server.js
@@ -3,16 +3,9 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 var csv = require('csv');
 var records = new Array();
+var output = "";
 
 app = express();
-
-mongoose.connect('mongodb://localhost/directory');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback() {
-  // code goes here
-});
-
 
 csv(records)
   .from.stream(fs.createReadStream('data' + '/directory.csv'), {
@@ -23,6 +16,48 @@ csv(records)
 
   //console.log(row);
 });
+
+// This large chunk of commented code saved all the csv
+// records into mongodb as part of it's first-time execution.
+// Now that we have our records, it is not needed anymore, 
+// but I'll leave it here for now.
+
+/*mongoose.connect('mongodb://localhost/directory');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+  // defining schema
+  var schoolSchema = mongoose.Schema({
+      countyName: String,
+      schoolName: String,
+      address: String,
+      city: String,
+      state: String,
+      zip: String,
+      phone: String
+    });
+
+  // defining model
+  var School = mongoose.model('School', schoolSchema);
+  var item;  
+
+  for(var i = 0; i < records.length; i++) {
+    item = new School( {
+      countyName: records[i][' County Name'],
+      schoolName: records[i][' School Name'],
+      address: records[i][' Address'],
+      city: records[i][' City'],
+      state: records[i][' State'],
+      zip: records[i][' Zip'],
+      phone: records[i][' Phone']
+    });
+
+    item.save(function (err, item) {
+      if (err) return console.error(err);
+    });
+  }
+});*/
+
 
 app.get( '/', function(req, res) {
   res.send(records);
