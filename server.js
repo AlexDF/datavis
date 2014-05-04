3faa7682eb959f7f489bedd26bc7d21ba30cb601
Alexandre DeFreitas <abd9@njit.edu>
2014-05-04 18:04:41 -0400

    added ability to retrieve ratings statistics from mongodb and return to index.ejs as an array in order to provide the data for the visualizations

--- server.js
+++ server.js
@@ -5,11 +5,29 @@ var csv = require('csv');
 var records = new Array();
 var output = "";
 var bodyParser = require('body-parser');
+var ratingStats = new Array();
 
 app = express();
 app.use(bodyParser());
 
-csv(records)
+mongoose.connect('mongodb://localhost/directory/');
+var db = mongoose.connection;
+
+var schoolSchema = mongoose.Schema({
+      countyName: String,
+      schoolName: String,
+      address: String,
+      city: String,
+      state: String,
+      zip: String,
+      phone: String,
+      grade: String
+    });
+
+
+var School = mongoose.model('School', schoolSchema);
+
+/*csv(records)
   .from.stream(fs.createReadStream('data' + '/directory.csv'), {
   columns: true
 })
@@ -24,7 +42,7 @@ csv(records)
 // Now that we have our records, it is not needed anymore, 
 // but I'll leave it here for now.
 
-/*mongoose.connect('mongodb://localhost/directory');
+mongoose.connect('mongodb://localhost/directory');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback() {
@@ -36,7 +54,8 @@ db.once('open', function callback() {
       city: String,
       state: String,
       zip: String,
-      phone: String
+      phone: String,
+      rating: String
     });
 
   // defining model
@@ -46,51 +65,57 @@ db.once('open', function callback() {
   for(var i = 0; i < records.length; i++) {
     item = new School( {
       countyName: records[i][' County Name'],
-      schoolName: records[i][' School Name'],
+      schoolName: records[i][' School Name'].toUpperCase(),
       address: records[i][' Address'],
       city: records[i][' City'],
       state: records[i][' State'],
       zip: records[i][' Zip'],
-      phone: records[i][' Phone']
+      phone: records[i][' Phone'],
+      rating: records[i]['Rating']
     });
 
     item.save(function (err, item) {
       if (err) return console.error(err);
     });
   }
+
+  console.log("Completed!");
 });*/
 
 
 app.get( '/', function(req, res) {
-  res.render('index.ejs', {result: ''});
+  res.render('index.ejs', {name: '', address: '', city: '', state: ''});
 });
 
 app.post('/findschool', function(req, res) {
-  mongoose.connect('mongodb://localhost/directory/');
-  var db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'connection error:'));
-  db.once('open', function callback() {
-   /* var schoolSchema = mongoose.Schema({
-      countyName: String,
-      schoolName: String,
-      address: String,
-      city: String,
-      state: String,
-      zip: String,
-      phone: String
-    });*/
+    School.find({ schoolName: req.body.school_name }, function(err, school) {
+      if(err) {console.log('Error: ' + err);}
+      else {
+        var match = school[0];
+        res.render('index.ejs', {name: match.schoolName, address: match.address, 
+          city: match.city+',', state: match.state});
+      }
+    }); // end School.find()
+}); // end app.post('/findschool',)
 
-
-    //var schools = mongoose.model('schools', schoolSchema);
- 
-    var match = db.schools.find( {schoolName: req.body.school_name} );
-
-    res.render('index.ejs', {result: match.length});
-  });
-  //res.render('index.ejs', {result: match});
-});
+app.get('/getstats', function(req, res) {
+  for (var i = 1; i <= 10; i++) {
+    School.find({ rating: i.toString() }, function(err, school) {
+      if(err) {console.log('Error: ' + err);}
+      else {
+        ratingStats.push(school.length);
+        //console.log(school);
+      }
+    }); // end School.find
+  } // end for loop
+  
+  res.render('index.ejs', {ratings: ratingStats});
+}); // end app.get('/getstats')
 
 app.listen(3000);
+
+
+
 
 
 
